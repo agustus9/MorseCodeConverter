2ea0cdcd38e1d86f83cd17425813f88aadce91f1
Mack <amack@proforma.com>
2018-04-04 14:23:59 -0400

    Created Dictionary

--- MorseCodeConverter/Program.cs
+++ MorseCodeConverter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,24 @@ namespace MorseCodeConverter
     {
         static void Main(string[] args)
         {
+            var morseCode = new Dictionary<String, String>();
+            const string MORSE_CODE_PATH = "../../morse.csv";
+            using (var reader = new StreamReader(MORSE_CODE_PATH))
+            {
+                while (reader.Peek() > -1)
+                {
+                    var line = reader.ReadLine();
+                    Console.WriteLine(line);
+                    var splitLine = line.Split(',');
+                    var key = splitLine[0];
+                    var value = splitLine[1];
+                    Console.WriteLine(splitLine);
+                    Console.WriteLine($"The key/value pair is {key}/{value}");
+                    morseCode.Add(key, value);
+                }
+                Console.WriteLine(morseCode.Count);
+            }
+            Console.ReadLine();
         }
     }
 }
